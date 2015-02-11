29f81f97427ddeea07366900b188e8580586bb9e
AmiruAsyraf <amiruasyraf@gmail.com>
2015-02-11 12:22:01 +0800

    second commit

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -8,10 +8,10 @@ post '/create' do
 # p params[:num_of_choices]
   choices = params[:num_of_choices].to_i
   choice_array = []
-  # byebug
+
   new_section = ""
   for i in 1..choices
-    new_section += "Choice #{i}: <textarea name=\"choice_array[\" + i +\"]\" cols=\"40\" rows=\"2\"></textarea><br/><br/>"
+    new_section += "Choice #{i}: <textarea name=\"choice_#{i}\" cols=\"40\" rows=\"2\"></textarea><br/><br/>"
   end
   new_section
 
@@ -23,9 +23,16 @@ end
 # end
 
 post "/create/done" do
-  byebug
-  p params[:survey_title]
-  p params[:question]
-  p params[:choice_array[0]]
+  # byebug
+  survey_title = params[:survey_title]
+  new_survey = Survey.create(title: survey_title)
+
+  question_text = params[:new_question]
+  new_question = new_survey.questions.create(text: question_text)
+
+  num_of_choices = params[:num_of_choices]
+  for i in 1..num_of_choices
+    new_question.choices.create(answer: params[:choice_i])
+  end
 
 end
